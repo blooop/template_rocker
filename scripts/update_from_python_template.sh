3006e12c8e845ccddc60c19661f58cfc515488a9
Austin Gregg-Smith <blooop@gmail.com>
2024-09-18 14:15:43 +0100

    add: secondary update from template script

--- scripts/update_from_python_template.sh
+++ scripts/update_from_python_template.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-#THIS IS THE CORRECT TEMPLATE UPDATE SCRIPT, NOT update_from_python_template.sh
+#THIS IS USED TO UPDATE THIS TEMPLATE FROM THE MAIN PYTHON TEMPLATE https://github.com/blooop/python_template.  YOU SHOULD NOT NEED TO USE THIS, PLEASE USE update_from_template.sh INSTEAD IF YOU WANT TO PULL UPDATES FROM TEMPLATE_ROCKER
 
 git config --global pull.rebase false
-git remote add template https://github.com/blooop/template_rocker.git
+git remote add template https://github.com/blooop/python_template.git
 git fetch --all
 git checkout main && git pull origin main
 git checkout -B feature/update_from_template; git pull
